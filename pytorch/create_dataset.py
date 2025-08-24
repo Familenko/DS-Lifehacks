52e3173bb74df3d372474c96f75a9939ebb70ed1
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2025-08-24 14:27:04 +0300

    fix bug with scaler in create_dataset

--- pytorch/create_dataset.py
+++ pytorch/create_dataset.py
@@ -1,25 +1,18 @@
 import numpy as np
 import pandas as pd
 
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-
 import torch
 from torch.utils.data import Dataset
 
 
 class CreateDataset(Dataset):
-    def __init__(self, X, y, scaler='StandardScaler'):        
+    def __init__(self, X, y, scaler=None):
         self.X = X
         self.y = y
-        self.scaler = None
+        self.scaler = scaler
 
-        if scaler == 'StandardScaler':
-            self.scaler = StandardScaler()
-            self.X = self.scaler.fit_transform(self.X)
-        elif scaler == 'MinMaxScaler':
-            self.scaler = MinMaxScaler()
-            self.X = self.scaler.fit_transform(self.X)
-
+        if self.scaler:
+            self.X = self.scaler.transform(self.X)
 
     def __len__(self):
         return len(self.y)
